f254df0918c2543f627732c2899b4bee2a9ed463
anupkumar104 <anupsinghrawat2005@gmail.com>
2025-11-03 21:32:31 +0530

    data type program

---
+++ datatype.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // int x=2;
+    // long long l2=1300000000;
+    // float y=12.90;
+    // string s1, s2 ;
+    // cin >> s1 >> s2;
+    // cout << s1 << " "<< s2;
+    string s;
+   getline(cin , s);
+   cout<<s;
+}
