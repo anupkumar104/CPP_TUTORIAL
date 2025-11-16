494ce1b5508e381c6bd7aff57312f916d8afd510
ANUP KUMAR <anupsaroj104@gmail.com>
2025-11-16 11:31:44 +0530

    Add files via upload

---
+++ p7.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    // for (int  i = 1; i<7 ;i++)
+    // {
+    //     cout<<"i="<<i << "\n";
+    // }
+    // int i =1;
+    // while (i<7)
+    // {
+    //    cout<<"i="<<i << "\n";
+    //    i++;
+    // }
+    int i =1;
+    do{
+       cout<<"i="<<i << "\n";
+       i++;
+    }
+    while (i<7);
+   
+    
+  
+}
