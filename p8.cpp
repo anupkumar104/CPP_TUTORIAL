601b2b35c621b5c4a4b05e39d9ee352c4136226a
ANUP KUMAR <anupsaroj104@gmail.com>
2025-11-16 11:31:13 +0530

    Add files via upload

---
+++ p8.cpp
@@ -0,0 +1,20 @@
+// #include<iostream>
+// using namespace std;
+// int sum(int num1,  int num2){
+//     int num3 =num1+num2;
+//     return num3;
+// }
+// int main(){
+//     int s=sum(5,6);
+//     cout<<"sum="<<s;
+// }
+#include<iostream>
+using namespace std;
+void  sum(int num1,  int num2){
+    int num3 =num1+num2;
+    cout<< num3;
+}
+int main(){
+sum(5,6);
+    
+}
